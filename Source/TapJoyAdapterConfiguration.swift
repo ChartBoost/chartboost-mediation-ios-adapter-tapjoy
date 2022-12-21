f23c1fe28bfc1e871eb2d3a6cca5ed53e4dc6ac0
Daniel Barros <daniel.barros@chartboost.com>
2022-12-21 15:57:30 +0100

    Make configuration class compatible with Obj-C

--- Source/TapJoyAdapterConfiguration.swift
+++ Source/TapJoyAdapterConfiguration.swift
@@ -6,21 +6,23 @@
 import Tapjoy
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
-public class TapJoyAdapterConfiguration {
+@objc public class TapJoyAdapterConfiguration: NSObject {
     
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
-    public static var testMode: Bool = false {
+    @objc public static var testMode: Bool = false {
         didSet {
             Tapjoy.setDebugEnabled(testMode)
+            print("TapJoy SDK test mode set to \(testMode)")
         }
     }
     
     /// Flag that can optionally be set to enable the partner's verbose logging.
     /// Disabled by default.
-    public static var verboseLogging: Bool = false {
+    @objc public static var verboseLogging: Bool = false {
         didSet {
             Tapjoy.setDebugEnabled(verboseLogging)
+            print("TapJoy SDK verbose logging set to \(verboseLogging)")
         }
     }
 }
