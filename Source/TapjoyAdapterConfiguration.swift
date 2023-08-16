60f4c7f65c9d834154323d1df023639f730fc52e
Ray Graham <rayg@bitwisedesign.com>
2023-08-16 12:14:35 -0700

    HB-5933: use os_log instead of print (#34)
    * HB-5933: use os_log instead of print
    
    * Apply suggestions from code review

--- Source/TapjoyAdapterConfiguration.swift
+++ Source/TapjoyAdapterConfiguration.swift
@@ -4,16 +4,21 @@
 // license that can be found in the LICENSE file.
 
 import Tapjoy
+import os.log
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class TapjoyAdapterConfiguration: NSObject {
     
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.tapjoy", category: "Configuration")
+
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
     @objc public static var testMode: Bool = false {
         didSet {
             Tapjoy.setDebugEnabled(testMode)
-            print("Tapjoy SDK test mode set to \(testMode)")
+            if #available(iOS 12.0, *) {
+                os_log(.debug, log: log, "Tapjoy SDK test mode set to %{public}s", "\(testMode)")
+            }
         }
     }
     
@@ -22,7 +27,9 @@ import Tapjoy
     @objc public static var verboseLogging: Bool = false {
         didSet {
             Tapjoy.setDebugEnabled(verboseLogging)
-            print("Tapjoy SDK verbose logging set to \(verboseLogging)")
+            if #available(iOS 12.0, *) {
+                os_log(.debug, log: log, "Tapjoy SDK verbose logging set to %{public}s", "\(verboseLogging)")
+            }
         }
     }
 }
